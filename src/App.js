b292341a2cb4afb1d3fc9d09cac545ad98482d3c
Mihaela Corina <phoenixco@users.noreply.github.com>
2019-10-29 11:39:26 +0000

    initial setup
    add high hierarchy component and initial layout

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React, { Component } from "react";
+import Layout from "./components/Layout/Layout";
+
+class App extends Component {
+  render() {
+    return (
+      <Layout>
+        <p>hola</p>
+      </Layout>
+    );
+  }
+}
+
+export default App;
