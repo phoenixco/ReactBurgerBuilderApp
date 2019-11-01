0d1cbe01079b166cd03a3e220b199a6f7b3d82cd
Mihaela Corina <phoenixco@users.noreply.github.com>
2019-11-01 16:06:26 +0000

    Add files via upload

--- src/App.js
+++ src/App.js
@@ -1,13 +1,16 @@
-import React, { Component } from "react";
-import Layout from "./components/Layout/Layout";
-import BurgerBuilder from "./containers/BurgerBuilder/BurgerBuilder";
+import React, { Component } from 'react';
+
+import Layout from './hoc/Layout/Layout';
+import BurgerBuilder from './containers/BurgerBuilder/BurgerBuilder';
 
 class App extends Component {
-  render() {
+  render () {
     return (
-      <Layout>
-        <BurgerBuilder />
-      </Layout>
+      <div>
+        <Layout>
+          <BurgerBuilder />
+        </Layout>
+      </div>
     );
   }
 }
